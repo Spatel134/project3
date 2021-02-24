ecd011b009c4b423f9c95e1f562ceae6087c528c
Sahil Patel <Patelsaho@gmail.com>
2021-02-24 17:44:29 -0500

    Move models folder from client to server

--- client/models/index.js
+++
@@ -1,5 +0,0 @@
-// Exporting an object containing all of our models
-module.exports = {
-    Item: require("./Item"),
-    Location: require("./Location")
-  };
--- models/item.js
+++ models/item.js
@@ -1,6 +1,6 @@
-const mongoose = require("moongoose");
+const mongoose = require("mongoose");
 
-const Schema = moongoose.Schema;
+const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
   name: { type: String, required: true },
