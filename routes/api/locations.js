2cda578949a7282f89de2af454fdb9982faf33e2
Sahil Patel <Patelsaho@gmail.com>
2021-02-24 17:46:04 -0500

    staging routes

---
+++ routes/api/locations.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const locationsController = require("../../controllers/locationsController");
+
+// Matches with "/api/locations"
+router
+  .route("/")
+  .get(locationsController.findAll)
+  .post(locationsController.create);
+
+// Matches with "/api/locations/:id"
+router
+  .route("/:id")
+  .get(locationsController.findById)
+  .put(locationsController.update)
+  .delete(locationsController.remove);
+
+router
+  .route("/:id/addItems")
+  .get(locationsController.findByIdWithItems)
+  .post(locationsController.createItem);
+
+module.exports = router;
