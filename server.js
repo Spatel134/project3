a114331dd6d963048cac1d51285ea53ef528d668
Andre Diop <itsjustandre@gmail.com>
2021-02-24 14:20:11 -0500

    Removed routes
    - Removed instance to older folder

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 
@@ -31,7 +30,6 @@ app.get("/api/config", (req, res) => {
   });
 });
 
-app.use(routes);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
