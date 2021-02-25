06ebecc2da8d03e4efd429d23ab9217a69b4fdbb
Andre Diop <itsjustandre@gmail.com>
2021-02-25 10:44:34 -0500

    Imported Materialize CSS
    - initalized materilized components in useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
 // import { useEffect } from "react";
 // import axios from "axios";
 // import { response } from "express";
+// import ItemCreate from "./components/ItemCreate/index";
+// import ItemUpdate from "./components/ItemUpdate/index";
+import M from "materialize-css";
+import { useEffect } from "react";
 
 function App() {
-//   useEffect(() => {
-//     axios
-//       .get("api/config")
-//       .then((response) => {
-//         console.log(response.data);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   }, []);
+  useEffect(() => {
+    M.AutoInit();
+  });
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      
+    </>
   );
 }
 
