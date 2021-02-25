08ebd4666dbc9b434eb233bdf81d8dea32b097c9
Alsharif Nahas <eid_alshareef@hotmail.com>
2021-02-25 15:58:58 -0500

    added the seeds file

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,40 +1,72 @@
 let mongoose = require("mongoose");
-let db = require("../client/models");
+let db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project3", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
 });
 
-let location =
-{
-    name: "Bed-stuy Fridge",
-    street: "160 Quincy",
-    city: "Brooklyn",
-    state: "New York",
-    zipcode: "11216",
 
-};
 
-let item =
-{
-    name: "Peanut Butter",
-    category: "Pantry",
-    addedBy: "James",
-    date: new Date(),
-    expiration: new Date(),
+let item = [
+    {
+        name: "Peanut Butter",
+        category: "Pantry",
+        addedBy: "James",
+        date: new Date(Date.now()),
+        expiration: new Date(Date.now()),
 
-}
+    },
+    {
+        name: "Whole Milk",
+        category: "Fridge",
+        addedBy: "James",
+        date: new Date(Date.now()),
+        expiration: new Date(Date.now()),
 
-db.Workout.deleteMany({})
-    .then(() => db.Workout.collection.insertMany(location, item))
-    .then(data => {
-        console.log(data.result.n + " records inserted!");
+    }
+
+]
+
+let locations = [
+    {
+        name: "Bed-stuy Fridge",
+        street: "160 Quincy",
+        city: "Brooklyn",
+        state: "New York",
+        zipcode: "11216",
+        items: []
+
+    },
+];
+
+db.Location.deleteMany({})
+    .then(() => db.Location.collection.insertMany(locations))
+    .then(info => {
+        console.log(locations)
+        console.log("in the second call back")
+        console.log(info.result.n + " records inserted!");
         process.exit(0);
+
     })
     .catch(err => {
         console.error(err);
         process.exit(1);
+    })
+
+db.Item.deleteMany({})
+    .then(() => db.Item.collection.insertMany(item))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1)
     });
+
+
+
