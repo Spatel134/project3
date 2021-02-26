90d287908f54f614278442167fc9d565bc2ea73d
Caleb Glorvigen <jcglorvi@gmail.com>
2021-02-26 00:10:53 -0500

    Added an axios call for route '/api/config' for testing purposes

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,27 @@
 // import ItemUpdate from "./components/ItemUpdate/index";
 import M from "materialize-css";
 import { useEffect } from "react";
+import axios from "axios";
 
-import Home from './pages/Home'
+import Home from "./pages/Home";
 
 function App() {
   useEffect(() => {
     M.AutoInit();
-  });
+    axios
+      .get("/api/config")
+      .then((response) => {
+        console.log(`AXIOS CALL`,response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-
     <div className="App">
-      <Home/>
+      <Home />
     </div>
-
   );
 }
 
