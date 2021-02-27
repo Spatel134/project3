74ce3c1f8c9bb0aa9ba9cc67e15fb2557c800e0b
Alsharif Nahas <eid_alshareef@hotmail.com>
2021-02-27 13:49:51 -0500

    the funcitonality if the api call in the view locations and single location is done

--- client/src/pages/ViewSingleLocation/SingleLocation.js
+++ client/src/pages/ViewSingleLocation/SingleLocation.js
@@ -4,60 +4,36 @@ import axios from 'axios';
 import Sidebar from '../../components/Sidebar';
 
 const SingleLocation = () => {
+    const [location, setLocation] = useState({});
+    const [items, setItems] = useState([]);
 
     useEffect(() => {
         loadItems();
     }, []);
 
     const loadItems = () => {
-        axios.get("/api/items").then(res => {
-            console.log(res)
+        const id = window.location.pathname.split("/");
+
+        axios.get("/api/locations/" + id[3]).then(({ data: { items, ...locationInfo } }) => {
+            setLocation(locationInfo);
+            setItems(items)
         });
     }
-
-
-    const items =
-        [
-            {
-                name: "Peanut Butter",
-                category: "Pantry",
-                addedBy: "1/1/2021",
-                date: "11/13/2020",
-                expiration: "02/01/2022"
-            },
-            {
-                name: "Whole Milk",
-                category: "Fridge",
-                addedBy: "11/15/2020",
-                date: "11/13/2020",
-                expiration: "11/20/2020"
-            }
-        ]
-    let locations =
-    {
-        name: "Bed-stuy Fridge",
-        street: "160 Quincy",
-        city: "Brooklyn",
-        state: "New York",
-        zipcode: "11216",
-        items: []
-
-    }
-
 
 
     return (
         <>
             <Sidebar />
+
             <div className="container">
                 <div className="row">
                     <div className="col s12">
-                        <h4 className="center-align">{locations.name}</h4>
+                        <h4 className="center-align">{location.name}</h4>
                     </div>
                     <div className="col s12">
                         <p className="center-align">
                             Located in:{" "}
-                            {`${locations.street}, ${locations.city}, ${locations.state}, ${locations.zipcode}`}
+                            {`${location.street}, ${location.city}, ${location.state}, ${location.zipcode}`}
                         </p>
                     </div>
                 </div>
--- controllers/locationsController.js
+++ controllers/locationsController.js
@@ -3,15 +3,22 @@ console.log("Hello!");
 // Defining methods for the locationsController
 module.exports = {
   findAll: function (req, res) {
+
     db.Location.find(req.query)
       .sort({ date: -1 })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   findById: function (req, res) {
-    db.Location.findById(req.params.id)
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => res.status(422).json(err));
+    db.Location.findOne({ _id: req.params.id })
+      .populate("items")
+      .then(info => {
+        res.json(info);
+      }).catch(err => {
+        console.log(err)
+      })
+
+
   },
   create: function (req, res) {
     db.Location.create(req.body)
