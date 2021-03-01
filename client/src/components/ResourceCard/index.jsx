0c905080918c5df174f97077c39b7ddcad3ce0f6
Andre Diop <itsjustandre@gmail.com>
2021-03-01 11:49:28 -0500

    addded 4th card

--- client/src/components/ResourceCard/index.jsx
+++ client/src/components/ResourceCard/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import M from 'materialize-css'
+
 
 const datas = [
   {
@@ -15,29 +15,26 @@ const datas = [
     picture:
       'https://i0.wp.com/www.okcps.org/cms/lib/OK01913268/Centricity/Domain/108/Grants/Apply%20for%20a%20Grant.png?resize=297%2C217&ssl=1',
     info:
-      'Mini grants are availiable to those looking to start their own community fridge',
+      'Mini grants are available to those looking to start their own community fridge',
     link:
       'https://docs.google.com/forms/d/e/1FAIpQLScv-7v2BdwyZQ4s7UXOioSqs3HXntpmrBbM45ePxWQA1Go5RA/viewform?usp=sf_link'
   },
   {
     name: 'Food Safety guidelines',
-    picture: 'https://lowrysolutions.com/wp-content/uploads/2017/03/Food-Safety-Stamp.png',
+    picture:
+      'https://lowrysolutions.com/wp-content/uploads/2017/03/Food-Safety-Stamp.png',
     info:
-      'Food safety plays an important role in the reputation of an establishment. Implementing good, safe practices will ensure you are doing all you can to avoid getting anyone sick. ',
-    link:
-      'https://www.cdc.gov/foodsafety/keep-food-safe.html'
+      'Implementing good, safe practices will ensure you are doing all you can to avoid getting anyone sick. Food safety plays an important role in the reputation of an establishment.  ',
+    link: 'https://www.cdc.gov/foodsafety/keep-food-safe.html'
   },
   {
     name: 'Food Safety guidelines',
-    picture: 'https://lowrysolutions.com/wp-content/uploads/2017/03/Food-Safety-Stamp.png',
+    picture: 'http://jmderby.com/wp-content/uploads/2016/01/Know-the-rules.jpg',
     info:
-      'The Bratislava FREEdge team made some printable resources that can be used to inform members of the community about your fridge. ',
-    link:
-      'https://freedge.wixsite.com/freedge/copy-of-get-help'
+      'Here you will find printable resources that can be used to inform members of the community about your fridge rules and regulations of your fridge. ',
+    link: 'https://freedge.wixsite.com/freedge/copy-of-get-help'
   }
 ]
-
-
 
 const index = () => {
   return (
