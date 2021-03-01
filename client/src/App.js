d45b20964fc46a04a0f5bf0dd3b63565afd792d0
Andre Diop <itsjustandre@gmail.com>
2021-03-01 13:39:35 -0500

    Creating an About page

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,18 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SingleLocation from "./pages/ViewSingleLocation/SingleLocation";
 import ViewLocations from "./pages/ViewLocations/ViewLocations";
-import ItemCreate from './components/ItemCreate'
+import ItemCreate from "./components/ItemCreate";
 import M from "materialize-css";
 import { useEffect } from "react";
-
+import ResourceCard from "../src/components/ResourceCard";
+import Resources from "./pages/Resources";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   useEffect(() => {
     M.AutoInit();
-    // axios
-    //   .get("/api/config")
-    //   .then((response) => {
-    //     console.log(`AXIOS CALL`,response.data);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
   }, []);
 
   return (
@@ -30,15 +20,14 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/resources" component={Resources} />
+          <Route exact path="/about" component={About} />
           <Route path="/locations" component={ViewLocations} />
           <Route path="/api/locations/:id" component={SingleLocation} />
           <Route path="/forms" component={ItemCreate} />
         </Switch>
       </Router>
-
-
-    </div >
-
+    </div>
   );
 }
 
