032bafe76adf9796cf5cc1a1f622b60ab27b22ac
Andre Diop <itsjustandre@gmail.com>
2021-03-02 14:10:49 -0500

    Removed custom sizing function
    Added a 3 col to display 3 items

--- client/src/pages/ViewLocations/ViewLocations.js
+++ client/src/pages/ViewLocations/ViewLocations.js
@@ -15,15 +15,6 @@ const ViewLocations = () => {
       setLocations(res.data);
     });
   };
-  let colSize;
-  if (12 % locations.length === 0 && 12 > locations.length) {
-    console.log("fisrt statment");
-    colSize = 12 / locations.length;
-    console.log(colSize);
-  } else {
-    console.log("second statment");
-    colSize = 6;
-  }
 
   return (
     <>
@@ -31,27 +22,19 @@ const ViewLocations = () => {
 
       <div className="container">
         <div className="row">
-          <div className="col s12">
-            <h4 className="center-align">Scroll for Different Fridges</h4>
-          </div>
-          <div className="row">
-            <div className="col s12">
-              {locations.map(
-                ({ name, _id, image, street, city, state, zipcode }) => (
-                  <Location
-                    name={name}
-                    id={_id}
-                    colSize={colSize}
-                    image={image}
-                    street={street}
-                    city={city}
-                    state={state}
-                    zipcode={zipcode}
-                  />
-                )
-              )}
-            </div>
-          </div>
+          {locations.map(
+            ({ name, _id, image, street, city, state, zipcode }) => (
+              <Location
+                name={name}
+                id={_id}
+                image={image}
+                street={street}
+                city={city}
+                state={state}
+                zipcode={zipcode}
+              />
+            )
+          )}
         </div>
       </div>
     </>
