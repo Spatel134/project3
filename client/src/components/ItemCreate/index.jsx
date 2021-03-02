1a9545126d640664045a748cc844554447967950
Sahil Patel <Patelsaho@gmail.com>
2021-03-01 19:06:10 -0500

    updated form

--- client/src/components/ItemCreate/index.jsx
+++ client/src/components/ItemCreate/index.jsx
@@ -1,159 +1,51 @@
-import React from 'react'
-import axios from 'axios'
-import { useState, useEffect } from 'react';
-import Header from '../Header/Header'
-import M from "materialize-css";
-
+import React from "react";
+import axios from "axios";
+import Header from "../Header/Header";
+import { useHistory } from "react-router-dom";
+import ItemForm from "../ItemForm/ItemForm";
 
 const ItemAdd = () => {
-  const [name, setName] = useState('')
-  const [category, setCategory] = useState('')
-  const [addedBy, setAddedBy] = useState('')
-  const [expiration, setExpiration] = useState('')
-  const [locations, setLocations] = useState([])
-  const [selectedLocation, setSelectedLocation] = useState('')
+  const history = useHistory();
 
-  useEffect(() => {
-    M.AutoInit();
+  const handleFormSubmit = (event, formObject) => {
+    event.preventDefault();
     axios
-      .get('api/locations', { responseType: 'json' })
-      .then(response => {
-        setLocations(response.data)
-        console.log(response.data)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }, [])
-  const handleFormSubmit = event => {
-    event.preventDefault()
+      .post("/api/items", formObject)
+      .then((response) => {
+        console.log(response.data);
 
-    // useEffect to get location id from an axios call
-    // set response to locationchoice state and use that to map[ over a dropdown choices]
-
-    axios
-      .post('/api/items', {
-        name,
-        category,
-        addedBy,
-        expiration,
-        selectedLocation
+        history.push(`/api/locations/${response.data._id}`);
       })
-      .then(response => {
-        console.log(response.data)
-
-        // Reset Form Inputs
-        setName('')
-        setCategory('')
-        setAddedBy('')
-        setExpiration('')
-        // Todo clear category
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <>
       <Header />
       <div>
-        <div className='container'>
-          <nav className='transparent waves-effect waves-cyan'> </nav>
-          <div className='row'>
-            <div className='col s6'>
+        <div className="container">
+          <nav className="transparent waves-effect waves-cyan"> </nav>
+          <div className="row">
+            <div className="col s6">
               <img
-                className='responsive-img'
-                src='   https://wpcdn.us-east-1.vip.tn-cloud.net/www.sactownmag.com/content/uploads/2020/10/121610343_2690149421252095_9180172192574381574_n.jpg'
-                alt='Colorfully designed community fridge'
+                className="responsive-img"
+                src="   https://wpcdn.us-east-1.vip.tn-cloud.net/www.sactownmag.com/content/uploads/2020/10/121610343_2690149421252095_9180172192574381574_n.jpg"
+                alt="Colorfully designed community fridge"
               />
             </div>
-            <div className='col s6'>
-              <form className='col s12' onSubmit={handleFormSubmit}>
-                <div className='input-field col s12  l12'>
-                  <input
-                    id='name'
-                    type='text'
-                    value={name}
-                    onChange={event => setName(event.target.value)}
-                    className='validate'
-                    placeholder='Nature Valley granola bar'
-                  />
-                  <label className='active' htmlFor='name'>
-                    {' '}
-                  Item Name
-                </label>
-                </div>
-                <div className='input-field col s12 l12'>
-                  <input
-                    id='addedBy'
-                    value={addedBy}
-                    onChange={event => setAddedBy(event.target.value)}
-                    type='text'
-                    className='validate'
-                    placeholder='johndough@gmail.com'
-                  />
-                  <label htmlFor='addedBy' className='active'>
-                    Added by
-                </label>
-                </div>
-                <div className='input-field col s12'>
-                  <input
-                    id='date'
-                    type='date'
-                    value={expiration}
-                    onChange={event => setExpiration(event.target.value)}
-                    className='validate'
-                  />
-                  <label className='active' htmlFor='expiration'>
-                    Expiration Date
-                </label>
-                </div>
-                <div className='input-field col s12'>
-                  <select
-                    value={category}
-                    onChange={event => setCategory(event.target.value)}
-                  >
-                    <option value='' disabled defaultValue>
-                      Choose your option
-                  </option>
-                    <option value='Frozen'>Frozen</option>
-                    <option value='Produce'>Produce</option>
-                    <option value='Pantry'>Pantry</option>
-                    <option value='Personal Care'>Personal Care</option>
-                  </select>
-                  <label>Category</label>
-                </div>
-                <div className='input-field col s12'>
-                  <select
-                    className="browser-default"
-                    onChange={event => setSelectedLocation(event.target.value)}
-                  >
-                    {/* 
-                  
-                  Behavior of the dropdown is strange, it is selecting a fridge but not getting
-                  the list of all of the fridges 
-                  
-                  */}
-                    <option value='' disabled defaultValue>
-                      Choose your option
-                  </option>
-                    {locations.map(location => (
-                      <option key={location._id} value={location._id}>
-                        {location.name}
-                      </option>
-                    ))}
-                  </select>
-                  <label className="active">Choose Location</label>
-                </div>
-                <button className='waves-effect waves-light btn'>Add Item</button>
-              </form>
+            <div className="col s6">
+              <ItemForm
+                handleFormSubmit={handleFormSubmit}
+                buttonText="Add Item"
+              />
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ItemAdd
+export default ItemAdd;
--- client/src/components/ItemUpdate/index.jsx
+++ client/src/components/ItemUpdate/index.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React from 'react';
+import axios from "axios";
+import { useHistory, useParams } from "react-router-dom";
+import ItemForm from "../ItemForm/ItemForm";
 
-const ItemAdd = () => {
+const ItemUpdate = () => {
+    const history = useHistory();
+    const { id } = useParams();
+
+    const handleFormSubmit = (event, formObject) => {
+        event.preventDefault();
+        axios
+          .post(`/api/items/${id}`, formObject)
+          .then((response) => {
+            console.log(response.data);
+    
+            history.push(`/items/${response.data._id}`);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      };
+
   return (
     <div>
       <div className='container'>
@@ -13,50 +33,10 @@ const ItemAdd = () => {
             />
           </div>
           <div className='col s6'>
-            <form className='col s12'>
-              <div className='input-field col s12  l12'>
-                <input
-                  id='name'
-                  type='text'
-                  className='validate'
-                  placeHolder='Nature Valley granola bar'
-                />
-                <label className='active' for='name'>
-                  {' '}
-                  Item Name
-                </label>
-              </div>
-              <div className='input-field col s12 l12'>
-                <input
-                  id='addedBy'
-                  type='text'
-                  className='validate'
-                  placeHolder='johndough@gmail.com'
-                />
-                <label className='active' for='addedBy'>
-                  Added by
-                </label>
-              </div>
-              <div className='input-field col s12'>
-                <input id='date' type='date' className='validate' />
-                <label className='active' for='expiration'>
-                  Expiration Date
-                </label>
-              </div>
-              <div className='input-field col s12'>
-                <select>
-                  <option value='' disabled selected>
-                    Choose your option
-                  </option>
-                  <option value='1'>Frozen</option>
-                  <option value='2'>Produce</option>
-                  <option value='3'>Pantry</option>
-                  <option value='4'>Personal Care</option>
-                </select>
-                <label>Category</label>
-              </div>
-              <a className='waves-effect waves-light btn'>Update Item</a>
-            </form>
+          <ItemForm
+                handleFormSubmit={handleFormSubmit}
+                buttonText="Update Item"
+              />
           </div>
         </div>
       </div>
@@ -64,4 +44,4 @@ const ItemAdd = () => {
   )
 }
 
-export default ItemAdd
+export default ItemUpdate;
