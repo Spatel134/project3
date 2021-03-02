29f1cf936234d48541292b9b22100c343503498f
Sahil Patel <Patelsaho@gmail.com>
2021-03-02 17:40:39 -0500

    Cleanup and removal of console logs

--- client/src/pages/ViewSingleLocation/SingleLocation.js
+++ client/src/pages/ViewSingleLocation/SingleLocation.js
@@ -52,7 +52,6 @@ const SingleLocation = () => {
               </thead>
 
               <tbody>
-                {console.log(items)}
                 {items.map((item) => (
                   <Item item={item} locationId={location._id} />
                 ))}
