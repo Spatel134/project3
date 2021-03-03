7da8cff1477304db8002d3edc713c78bc1d1d2cc
Andre Diop <itsjustandre@gmail.com>
2021-03-02 23:43:53 -0500

    Location is deleted
    - toast displayed on screen upon deletion
    - page refresh upon deletion

--- client/src/pages/Admin/index.js
+++ client/src/pages/Admin/index.js
@@ -16,6 +16,8 @@ const About = () => {
   const [image, setImage] = useState("");
   const [locations, setLocations] = useState([]);
 
+  const history = useHistory();
+
   useEffect(() => {
     M.AutoInit();
 
@@ -74,6 +76,20 @@ const About = () => {
           .catch((err) => {
             console.log(err);
           });
+  };
+
+  const handleDeleteClick = (_id, name) => {
+    console.log(name)
+    axios
+      .delete(`/api/locations/${_id}`)
+      .then((response) => {
+        console.log(response.data);
+        M.toast({ html: `${name} has been removed! `})
+        history.go(0);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -183,7 +199,7 @@ const About = () => {
         </div>
       </div> */}
       <div className="container">
-        {locations.map(({ name, city, state, street }) => (
+        {locations.map(({ name, city, state, street, _id }) => (
           <div className="row">
             <ul className="collection">
               <li className="collection-item avatar">
@@ -199,12 +215,31 @@ const About = () => {
                   <strong>{state} </strong>
                   <br />
                   <strong>Edit Location</strong>
-                  <a>
-                    <i className="material-icons">edit</i>
-                  </a>
+                  {/* 
+
+
+                  Todo: add below route to edit route. For now the  route just goes to the page.
+
+                     <Link to={`/api/locations/${_id}/edit`}>
+                  
+                  
+                  */}
+
+                  <Link to={`/api/locations/${_id}`}>
+                    <a href="javascript:void(0);">
+                      <i className="material-icons">edit</i>
+                    </a>
+                  </Link>
                 </p>
-                <a href="#!" className="secondary-content">
-                  <i className="material-icons">delete</i>
+                <a href="javascript:void(0);" className="secondary-content">
+                  <i
+                    className="material-icons"
+                    onClick={() => {
+                      handleDeleteClick(_id , name);
+                    }}
+                  >
+                    delete
+                  </i>
                 </a>
               </li>
             </ul>
