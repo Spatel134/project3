d4c17f20a4634bde1c226e1ec86515eb044c35ed
Sahil Patel <Patelsaho@gmail.com>
2021-03-03 21:02:53 -0500

    admin route

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,7 +4,9 @@ const locationRoutes = require("./locations");
 const itemRoutes = require("./items");
 
 router.use("/items", itemRoutes);
+router.use("/admin/items", itemRoutes);
 router.use("/locations", locationRoutes);
+router.use("/admin/locations", locationRoutes);
 router.use("/authentication", authentication);
 
 
