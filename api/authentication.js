98981962bc71ba8b7b2d62fe7236e11e830ef4a1
Caleb Glorvigen <jcglorvi@gmail.com>
2021-03-03 19:52:17 -0500

    Deleted repeated api files

--- api/authentication.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const authController = require("../../controllers/authController");
-
-
-router.route("/signup").post(authController.createdNewUser);
-router.route("/login").post(authController.loginUser);
-
-module.exports = router;
